a7654361d9c9d05fe73a41b06edc6f79b59b9e42
Ryze <459791911@qq.com>
2020-01-28 16:39:07 +0800

    fix: merge

--- draw/index.js
+++ draw/index.js
@@ -1,73 +1,77 @@
 
 var websocketGame = {
-    isDrawing:false,
-    startX:0,
-    startY:0,
-	
-    LINE_SEGMENT : 0,
-    CHAT_MESSAGE : 1,
-    GAME_LOGIC:2,
-    
-    WAITING_TO_START:0,
-    GAME_START:1,
-    GAME_OVER:2,
-    GAME_RESTART:3,
-    isTurnToDraw:false
+	isDrawing: false,
+	startX: 0,
+	startY: 0,
+
+	LINE_SEGMENT: 0,
+	CHAT_MESSAGE: 1,
+	GAME_LOGIC: 2,
+
+	WAITING_TO_START: 0,
+	GAME_START: 1,
+	GAME_OVER: 2,
+	GAME_RESTART: 3,
+	isTurnToDraw: false
 }
- 
- 
+
+
 var canvas = document.getElementById("drawing-pad");
 var ctx = canvas.getContext('2d');
- 
-function drawLine(ctx,x1,y1,x2,y2,thickness){
+
+function drawLine(ctx, x1, y1, x2, y2, thickness) {
 	ctx.beginPath();
-	ctx.moveTo(x1,y1);
-	ctx.lineTo(x2,y2);
+	ctx.moveTo(x1, y1);
+	ctx.lineTo(x2, y2);
 	ctx.lineWidth = thickness;
-	ctx.strokeStyle = "#444";
+	ctx.strokeStyle = "#fff";
 	ctx.stroke();
 }
- 
- 
-$(document).ready(function(){
- 
-	$('#drawing-pad').mousedown(function(e){
-		var canvasPosition = $(this).offset();
-		var mouseX = (e.pageX - canvasPosition.left) || 0;
-		var mouseY = (e.pageY - canvasPosition.top) || 0;
- 
+
+
+$(document).ready(function () {
+	var pad = document.getElementById('drawing-pad');
+	pad.addEventListener('touchstart', function (e) {
+		var touchPort = e.touches[0];
+
+		var mouseX = touchPort.pageX;
+		var mouseY = touchPort.pageY
+
 		websocketGame.startX = mouseX;
 		websocketGame.startY = mouseY;
 		websocketGame.isDrawing = true;
-	});
- 
-	$('#drawing-pad').mousemove(function(e){
-		if(websocketGame.isDrawing){
-			var canvasPosition = $(this).offset();
-			var mouseX = (e.pageX - canvasPosition.left) || 0;
-			var mouseY = (e.pageY - canvasPosition.top) || 0;
+	}, false);
+
+	let mv_x = 0,
+		mv_y = 0,
+		mv_touchPort = { pageX: 0, pageY: 0 };
+	pad.addEventListener('touchmove', function (e) {
+		
+		if (websocketGame.isDrawing) {
+			mv_touchPort = e.touches[0];
+			mv_x = mv_touchPort.pageX;
+			mv_y = mv_touchPort.pageY;
+			if (!(mv_x == websocketGame.startX && mv_y == websocketGame.startY)) {
+				drawLine(ctx, websocketGame.startX, websocketGame.startY, mv_x, mv_y, 1);
+				var data = {};
+				data.dataType = websocketGame.LINE_SEGMENT;
+
+				data.startX = websocketGame.startX;
+				data.startY = websocketGame.startY;
+				data.endX = mv_x;
+				data.endY = mv_y;
+				ws.send(JSON.stringify(data));
+
+				websocketGame.startX = mv_x;
+				websocketGame.startY = mv_y;
+			}
 		}
- 
-		if(!(mouseX == websocketGame.startX && mouseY == websocketGame.startY)){
-			drawLine(ctx,websocketGame.startX,websocketGame.startY,mouseX,mouseY,1);
- 
-			var data = {};
-			data.dataType = websocketGame.LINE_SEGMENT;
-			
-			data.startX = websocketGame.startX;
-			data.startY = websocketGame.startY;
-			data.endX = mouseX;
-			data.endY = mouseY;
-			ws.send(JSON.stringify(data));
- 
-			
-			websocketGame.startX = mouseX;
-			websocketGame.startY = mouseY;
-		}
+
+
 	});
- 
-	$('#drawing-pad').mouseup(function(e){
+
+	pad.addEventListener('touchend', function (e) {
 		websocketGame.isDrawing = false;
 	});
- 
+
 })
